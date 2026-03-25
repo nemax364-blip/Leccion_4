58b852e9dc4d032788a82ecb054b92d359118e08
Diego <nemax364@gmail.com>
2026-03-25 03:23:33 -0600

    Commit Leccion 4

--- app/src/main/java/com/example/happybithday/MainActivity.kt
+++ app/src/main/java/com/example/happybithday/MainActivity.kt
@@ -1,20 +1,20 @@
 package com.example.happybithday
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val rollButton: Button = findViewById(R.id.button)
+        rollButton.setOnClickListener {
+            val resultTextView: TextView = findViewById(R.id.textView2)
+            resultTextView.text = "6"
         }
     }
 }
