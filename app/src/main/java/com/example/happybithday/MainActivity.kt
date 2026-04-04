e2d00ed186acd66cb6673cfe6d5a0a67e0a75085
Diego <nemax364@gmail.com>
2026-04-04 05:24:43 -0600

    Commit Leccion 4

--- app/src/main/java/com/example/happybithday/MainActivity.kt
+++ app/src/main/java/com/example/happybithday/MainActivity.kt
@@ -13,8 +13,20 @@ class MainActivity : AppCompatActivity() {
 
         val rollButton: Button = findViewById(R.id.button)
         rollButton.setOnClickListener {
-            val resultTextView: TextView = findViewById(R.id.textView2)
-            resultTextView.text = "6"
+            rollDice()
         }
+    }
+
+    private fun rollDice() {
+        val dice = Dice(6)
+        val diceRoll = dice.roll()
+        val resultTextView: TextView = findViewById(R.id.textView2)
+        resultTextView.text = diceRoll.toString()
+    }
+}
+
+class Dice(private val numSides: Int) {
+    fun roll(): Int {
+        return (1..numSides).random()
     }
 }
